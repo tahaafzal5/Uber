0d0d11e6693256ae93d47b07816b1dc474603fd7
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-18 16:30:51 -0600

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Taha Afzal on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
