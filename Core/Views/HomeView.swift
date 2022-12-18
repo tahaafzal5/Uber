2b72622ec0855dec33f0831e17893db2afa50d05
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-18 16:38:52 -0600

    Initial Setup

---
+++ Core/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Uber
+//
+//  Created by Taha Afzal on 12/18/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
