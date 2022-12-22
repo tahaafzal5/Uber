fa04ed68ab25f5bab8b2582622d67b4321f1e5b6
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-22 11:56:07 -0600

    Add LocationSearchActivationView to Project

---
+++ Core/LocationSearch/View/LocationSearchActivationView.swift
@@ -0,0 +1,36 @@
+//
+//  LocationSearchActivationVie.swift
+//  Uber
+//
+//  Created by Taha Afzal on 12/22/22.
+//
+
+import SwiftUI
+
+struct LocationSearchActivationView: View {
+    var body: some View {
+		HStack {
+			Rectangle()
+				.fill(.black)
+				.frame(width: 8, height: 8)
+				.padding(.leading)
+			
+			Text("Where to?")
+				.foregroundColor(Color(.darkGray))
+			
+			Spacer()
+		}
+		.frame(width: UIScreen.main.bounds.width - 64, height: 50)
+		.background(
+			Rectangle()
+				.fill(.white)
+				.shadow(radius: 5 )
+		)
+    }
+}
+
+struct LocationSearchActivationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchActivationView()
+    }
+}
