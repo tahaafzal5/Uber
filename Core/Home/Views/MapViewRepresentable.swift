b70a3c3c6acd3073812250885a1152b40611851a
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-23 14:12:37 -0600

    Add Polylines to User's Destination

--- Core/Home/Views/MapViewRepresentable.swift
+++ Core/Home/Views/MapViewRepresentable.swift
@@ -27,6 +27,7 @@ struct MapViewRepresentable: UIViewRepresentable {
 	func updateUIView(_ uiView: UIViewType, context: Context) {
 		if let selectedLocationCoordinates = locationViewModel.selectedLocationCoordinates {
 			context.coordinator.addAndSelectAnnotation(withCoordinate: selectedLocationCoordinates)
+			context.coordinator.configurePolylines(withDestinationCoordinate: selectedLocationCoordinates)
 		}
 	}
 	
@@ -38,20 +39,42 @@ struct MapViewRepresentable: UIViewRepresentable {
 extension MapViewRepresentable {
 	
 	class MapCoordinator: NSObject, MKMapViewDelegate {
+		
+		// MARK: Properties
+		
 		let parent: MapViewRepresentable
+		var userLocationCoordinate: CLLocationCoordinate2D?
+		
+		// MARK: Constructor
 		
 		init(parent: MapViewRepresentable) {
 			self.parent = parent
 			super.init()
 		}
 		
+		// MARK: MKMapViewDelegate
+		
 		func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+			self.userLocationCoordinate = userLocation.coordinate
+			
 			let region = MKCoordinateRegion(
 				center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude),
 				span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
 			
 			parent.mapView.setRegion(region, animated: true)
 		}
+		
+		func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+			parent.mapView.removeOverlays(parent.mapView.overlays)
+			
+			let polylineOverlay = MKPolylineRenderer(overlay: overlay)
+			
+			polylineOverlay.strokeColor = .systemBlue
+			
+			return polylineOverlay
+		}
+		
+		// MARK: Helper Functions
 		
 		func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
 			parent.mapView.removeAnnotations(parent.mapView.annotations)
@@ -63,6 +86,36 @@ extension MapViewRepresentable {
 			parent.mapView.selectAnnotation(annotation, animated: true)
 			
 			parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+		}
+		
+		func configurePolylines(withDestinationCoordinate destinationCoordinate: CLLocationCoordinate2D) {
+			guard let userLocationCoordinate = self.userLocationCoordinate else { return }
+			
+			getDestinationRoute(from: userLocationCoordinate, to: destinationCoordinate) { route in
+				self.parent.mapView.addOverlay(route.polyline)
+			}
+		}
+
+		func getDestinationRoute(from userLocation: CLLocationCoordinate2D, to userDestination: CLLocationCoordinate2D, completion: @escaping(MKRoute) -> Void) {
+			let userLocationPlacemark = MKPlacemark(coordinate: userLocation)
+			let userDestinationPlacemark = MKPlacemark(coordinate: userDestination)
+			let request = MKDirections.Request()
+			
+			request.source = MKMapItem(placemark: userLocationPlacemark)
+			request.destination = MKMapItem(placemark: userDestinationPlacemark)
+			
+			let directions = MKDirections(request: request)
+			directions.calculate { response, error in
+				if let error = error {
+					print("Failed to get directions with error: \(error.localizedDescription)")
+					
+					return
+				}
+				
+				guard let route = response?.routes.first else { return }
+				
+				completion(route)
+			}
 		}
 	}
 }
