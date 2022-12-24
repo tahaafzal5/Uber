72d08bca3b269037e3122829f71015979b4836f2
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-23 22:39:27 -0600

    Present RideRequestView

--- Core/Home/Views/HomeView.swift
+++ Core/Home/Views/HomeView.swift
@@ -11,27 +11,34 @@ struct HomeView: View {
 	@State private var mapState = MapViewState.NO_INPUT
 	
     var body: some View {
-		ZStack(alignment: .top) {
-			MapViewRepresentable(mapState: $mapState)
-				.ignoresSafeArea()
-		
-			if mapState == .SEARCHING_FOR_LOCATION
-			{
-				LocationSearchView(mapState: $mapState)
-			}
-			else if mapState == .NO_INPUT
-			{
-				LocationSearchActivationView()
-					.padding(.top, 60)
-					.onTapGesture {
-						withAnimation(.spring()) {
-							mapState = .SEARCHING_FOR_LOCATION
+		ZStack(alignment: .bottom) {
+			ZStack(alignment: .top) {
+				MapViewRepresentable(mapState: $mapState)
+					.ignoresSafeArea()
+			
+				if mapState == .SEARCHING_FOR_LOCATION
+				{
+					LocationSearchView(mapState: $mapState)
+				}
+				else if mapState == .NO_INPUT
+				{
+					LocationSearchActivationView()
+						.padding(.top, 60)
+						.onTapGesture {
+							withAnimation(.spring()) {
+								mapState = .SEARCHING_FOR_LOCATION
+							}
 						}
-					}
+				}
+		
+				ActionButtonView(mapState: $mapState)
+					.padding(.leading, 20)
 			}
-	
-			ActionButtonView(mapState: $mapState)
-				.padding(.leading, 20)
+		}
+		
+		if mapState == .LOCATION_SELECTED {
+			RideRequestView()
+				.transition(.move(edge: .bottom))
 		}
     }
 }
