39a6238b5cf38bcc2b1f8b0cff7d9efa353f03fd
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-24 13:32:36 -0600

    Be Able to Select RideType

--- Core/Trips/RideRequestView.swift
+++ Core/Trips/RideRequestView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct RideRequestView: View {
+	
+	@State private var selectedRideType: RideType = .X
+	
     var body: some View {
 		VStack {
 			Capsule()
@@ -69,7 +72,7 @@ struct RideRequestView: View {
 								.resizable()
 								.scaledToFit()
 							
-							VStack(spacing: 4) {
+							VStack(alignment: .leading, spacing: 4) {
 								Text(rideType.description)
 									.font(.system(size: 16, weight: .semibold))
 								
@@ -78,9 +81,16 @@ struct RideRequestView: View {
 							}
 							.padding(8)
 						}
+						.scaleEffect(rideType == selectedRideType ? 1.05 : 1)
 						.frame(width: 112, height: 140)
-						.background(Color(.systemGroupedBackground))
+						.foregroundColor(rideType == selectedRideType ? .white : .black)
+						.background(Color(rideType == selectedRideType ? .systemBlue : .systemGroupedBackground))
 						.cornerRadius(10)
+						.onTapGesture {
+							withAnimation(.spring()) {
+								selectedRideType = rideType
+							}
+						}
 					}
 				}
 			}
